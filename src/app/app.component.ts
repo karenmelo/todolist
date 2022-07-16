572086fb550a44b9fb19f4bda1b6a5e72825f893
Karen Melo <karen_melo@live.com>
2022-07-16 18:59:18 -0300

    Alteracao de componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todolist';
+  public todos: any[] = [];
+  public title: String = 'Minhas tarefas';
+
+  constructor() {
+    this.todos.push('passear com o cachorro');
+    this.todos.push('ir ao supermercado');
+    this.todos.push('cortar o cabelo');
+  }
+
+  alteraTexto(){
+    this.title = 'Teste';
+  }
 }
