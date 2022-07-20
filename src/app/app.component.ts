e9bc04eb6a0c414898dfab360ff3bc41b7d19b10
Karen Melo <karen_melo@live.com>
2022-07-20 01:53:21 -0300

    Inclusao da pasta model e modificando o todo para dinamico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,44 @@
+import { Todo } from './../models/todo.model';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  public todos: any[] = [];
-  public title: String = 'Minhas tarefas';
 
-  constructor() {
-    this.todos.push('passear com o cachorro');
-    this.todos.push('ir ao supermercado');
-    this.todos.push('cortar o cabelo');
+export class AppComponent {
+  public todos: Todo[] = [];
+  public title: String = 'Minhas tarefas';
+  public form: FormGroup;
+
+
+  constructor(public fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
+
+    this.todos.push(new Todo(1, 'Passear com o cachorro', false));
+    this.todos.push(new Todo(2, 'Ir ao supermercado', false));
+    this.todos.push(new Todo(3, 'Cortar o cabelo', true));
+  }
+
+  remove(todo: Todo){
+    const index = this.todos.indexOf(todo);
+    if(index !== -1){
+        this.todos.splice(index, 1);
+      }
+  }
+
+  markAsDone(todo: Todo){
+    todo.done = true;
+  }
+  markAsUndone(todo: Todo){
+    todo.done = false;
   }
 }
